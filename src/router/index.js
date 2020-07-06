902a53eccd6b475883efe271c5bb077b6bc574f9
rhladchenko <ruslan.hladchenko@cpcs.ws>
2020-07-06 18:27:39 +0300

    new test project

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,52 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/login",
+    name: "login",
+    component: () => import('../views/Login'),
+    meta: { layout: "empty" }
+  },
+  {
+    path: "/categories",
+    name: "categories",
+    meta: { layout: "main" },
+    component: () => import("../views/Categories.vue")
+  },
+  {
+    path: "/detail-record",
+    name: "detail-record",
+    meta: { layout: "main" },
+    component: () => import("../views/DetailRecord")
+  },
+  {
+    path: "/history",
+    name: "history",
+    meta: { layout: "main" },
+    component: () => import("../views/History")
+  },
+  {
+    path: "/planning",
+    name: "planning",
+    meta: { layout: "main" },
+    component: () => import("../views/Planning")
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    meta: { layout: "main" },
+    component: () => import("../views/Profile")
+  },
+  {
+    path: "/record",
+    name: "record",
+    meta: { layout: "main" },
+    component: () => import("../views/Record")
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: () => import('../views/Register'),
+    meta: { layout: "empty" }
   }
 ];
 
